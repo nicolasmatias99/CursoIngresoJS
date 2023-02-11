fe1a68a3b4ae8a8a5ed32407cce433e238bd7bbe
Nicolas <costantininicolas2@gmail.com>
2023-02-11 19:49:44 -0300

    Hecho

--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -7,20 +7,61 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
+
+var numeroSecreto;
 var contadorIntentos;
+
+contadorIntentos = 0;
+
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+   
+  let minimo;
+  let maximo;
 
+  minimo = 1;
+  maximo = 100;
+
+	  //Genero el número RANDOM entre 1 y 100
+
+  numeroSecreto = Math.round(Math.random() * (maximo - minimo) + minimo);
+
+    //alert(numeroSecreto );
+
+   console.log (numeroSecreto);
 }
 
 function verificar()
 {
-	
-	
+  let numeroIngresado;
+  
+  
+  numeroIngresado = parseInt(document.getElementById("txtIdNumero").value);
+  
+
+  if (numeroIngresado == numeroSecreto)
+  {
+    contadorIntentos = contadorIntentos + 1;
+    alert("“Usted es un ganador!!! y en solo " + contadorIntentos + " intentos”");
+  }
+
+  else if (numeroIngresado < numeroSecreto)
+  {    
+    alert("falta.");
+    contadorIntentos = contadorIntentos + 1;
+  }
+
+  else 
+  {
+    alert("Se pasó.");
+    contadorIntentos = contadorIntentos + 1;
+  }
+
+  
+
+  document.getElementById("txtIdIntentos").value = contadorIntentos;
+    
 }
+
+//NO ME SUMA 1 EN EL CONTADOR DE INTENTOS! SOLUCIONAR.
